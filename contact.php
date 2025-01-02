97ed6c61ab9eabc663b9792f58bb261ae09836f0
Sabbir <93143691+s8rr@users.noreply.github.com>
2025-01-03 04:48:39 +0600

    contect.php added

---
+++ contact.php
@@ -0,0 +1,54 @@
+<?php include('layouts/header.php'); ?>
+    <!-- Map Begin -->
+    <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.9292210590997!2d107.63751111459267!3d-6.899068695014868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e79619932267%3A0x7b0db64c18423167!2sJl.%20Phh.%20Mustofa%2C%20Cikutra%2C%20Kec.%20Cibeunying%20Kidul%2C%20Kota%20Bandung%2C%20Jawa%20Barat!5e0!3m2!1sen!2sid!4v1666383278199!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <!-- Map End -->
+
+    <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="contact__text">
+                        <div class="section-title">
+                            <span>Information</span>
+                            <h2>MF Binary</h2>
+                            <p>Melayani pemesanan poster edukasi, mainan edukasi, dan tumbler dengan harga terjangkau dan bersaing, kami melayani Anda setiap hari Senin - Jumat pukul 08.00 - 17.00, dan hari Sabtu - Minggu pukul 10.00 - 17.00, jangan sungkan untuk menghubungi kami. Kepuasan Anda adalah pencapaian kami.</p>
+                        </div>
+                        <ul>
+                            <li>
+                                <h4>Cimahi</h4>
+                                <p>Jl. Ciawi Tali, No.19, Kec. Cisokan. <br />62 837 22626221</p>
+                            </li>
+                            <li>
+                                <h4>Developer Contact</h4>
+                                <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kurniaramadhanputra" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://id.linkedin.com/in/kurniaramadhanputra?trk=profile-badge"></a></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="contact__form">
+                        <form action="#">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <input type="text" placeholder="Name">
+                                </div>
+                                <div class="col-lg-6">
+                                    <input type="text" placeholder="Email">
+                                </div>
+                                <div class="col-lg-12">
+                                    <textarea placeholder="Message"></textarea>
+                                    <button type="submit" class="site-btn">Send Message</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
+
+<?php include('layouts/footer.php'); ?>
